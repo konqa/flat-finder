9db4d5c45a505a066ea9c1286e33f172a92a147e
Victor Bagu <victorbagu@gmail.com>
2018-12-30 21:11:45 +0200

    Updates

---
+++ precache-manifest.6481f1c13b8e19050636776ead4fbb84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff2bbd246f0068449dcf",
+    "url": "/flat-finder/static/js/runtime~main.ff2bbd24.js"
+  },
+  {
+    "revision": "3a30b80858193895268e",
+    "url": "/flat-finder/static/js/main.3a30b808.chunk.js"
+  },
+  {
+    "revision": "88ff7ed64e306ac5df74",
+    "url": "/flat-finder/static/js/1.88ff7ed6.chunk.js"
+  },
+  {
+    "revision": "3a30b80858193895268e",
+    "url": "/flat-finder/static/css/main.66bdf306.chunk.css"
+  },
+  {
+    "revision": "1608d215016a3296de81cea33a29e1bb",
+    "url": "/flat-finder/index.html"
+  }
+];
